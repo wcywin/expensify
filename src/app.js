4ae247215bf912e2087d477baa6d1864a60d2036
Wojtek <C:\Users\Wojtek i Marta\Desktop\IT\The BAT!\Gowny katalog poczty programu The Bat!>
2018-07-12 08:05:13 +0200

    add router folder and files

--- src/app.js
+++ src/app.js
@@ -1,50 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExponseDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is from my help component
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404!
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExponseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit" component={EditExpensePage}/>
-            <Route path="/help" component={HelpPage}/>
-            <Route component={NotFoundPage}/>
-        </Switch>
-    </BrowserRouter>
-);
-
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
