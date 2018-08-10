084732f6e6cac39e350f63e17fcf39512b7a9fc2
wcywin <wcywin@gmail.com>
2018-08-10 07:55:21 +0200

    add firebase to redux - beginning

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,10 +13,82 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().on('value', (snapshot) => {
-    const val = snapshot.val();
-    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-});
+export { firebase, database as default };
+
+
+
+//
+// // child_removed - another event apart from 'value'
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+//
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+//
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+//
+//         snapshot.forEach((childSnapshot) => {
+//              expenses.push({
+//                  id: childSnapshot.key,
+//                  ...childSnapshot.val()
+//              });
+//         });
+//
+//         console.log(expenses);
+//     });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+//
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//     description: "Rent",
+//     note: "August 2018",
+//     amount: 130000,
+//     createdAt: 9754862165484585
+// });
+//
+// database.ref('expenses').push({
+//     description: "Car",
+//     note: "July 2018",
+//     amount: 12500,
+//     createdAt: 9578561243256847
+// });
+//
+// database.ref('expenses').push({
+//     description: "MacBook",
+//     note: "June 2018",
+//     amount: 5500,
+//     createdAt: 9542548654627735
+// });
+
+
+
+
+// database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// });
 
 // const onValueChange = database.ref().on('value', (snapshot) => {
 //     console.log(snapshot.val());
@@ -82,20 +154,6 @@ database.ref().on('value', (snapshot) => {
 //     }).catch((e) => {
 //         console.log('Did not remove data', e);
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //
