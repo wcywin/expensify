f066fa39a0365511b71ab7b69b40efb4be80f731
wcywin <wcywin@gmail.com>
2018-08-12 22:10:00 +0200

    setup test firebase database env

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,9 +1,9 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: 'Wojciech',
-        //     age: 26
-        // });
+        resolve({
+            name: 'Wojciech',
+            age: 30
+        });
         reject('Something went wrong!');
     }, 4000);
 });
@@ -12,6 +12,14 @@ console.log('before');
 
 promise.then((data) => {
     console.log("1", data);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('This is my other promise');
+        }, 5000);
+    });
+}).then((str) => {
+    console.log('does this run?', str);
 }).catch((error) => {
     console.log('error:', error);
 });
