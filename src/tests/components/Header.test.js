99018e193612d752246998af7ca676f64fdb4f4d
wcywin <wcywin@gmail.com>
2018-08-17 07:49:33 +0200

    add startLogout in Header and test

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 // import toJSON from 'enzyme-to-json';
 // import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => { }}/>);
     expect(wrapper).toMatchSnapshot();
 
     // expect(toJSON(wrapper)).toMatchSnapshot();
@@ -16,3 +16,11 @@ test('should render Header correctly', () => {
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
+});
+
