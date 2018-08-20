f6bd2ba205cd78c288d628334c3e4a487018515a
wcywin <wcywin@gmail.com>
2018-08-20 22:35:35 +0200

    add first styling to the app

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="login-layout">
+        <div className='login-layout__box'>
+            <h1 className="login-layout__title">Expensify</h1>
+            <p>It's time to get your expenses under control.</p>
+            <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>
     </div>
 );
 
